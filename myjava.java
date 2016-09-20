c7cf1fe93d8b19d1e15b4993ed7349475d7f6a1e
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2016-09-20 09:10:25 +0300

    Added method

--- myjava.java
+++ myjava.java
@@ -2,6 +2,7 @@
 public class main {
     public static void main(String[] args) { 
     	Product product = new Product("Edam", 3.3, 120);
+		product.changeBalance(200);
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
      }
@@ -22,5 +23,9 @@ public class main {
  
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
+	}
+	
+	public void changeBalance(int value) {
+		amount = value;
 	}
 }
