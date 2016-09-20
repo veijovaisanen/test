f68dbf1479a9ab287c8f7745a0534042b5b48c77
veijovaisanen <veijo.vaisanen@oamk.fi>
2016-09-20 09:12:14 +0300

    Update myjava.java

--- myjava.java
+++ myjava.java
@@ -2,8 +2,6 @@
 public class main {
     public static void main(String[] args) { 
     	Product product = new Product("Edam", 3.3, 120);
-		product.changePrice(4.1);
-		product.changeBalance(200);
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
      }
@@ -19,10 +17,7 @@ public class main {
 		this.amount = amount;
 	}
 	
-	public void changePrice(int value) {
-		price = value;
-	}
-	public double countValue() {
+ 	public double countValue() {
 		return(amount * price);
 	}
  
@@ -30,7 +25,4 @@ public class main {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
 	}
 	
-	public void changeBalance(int value) {
-		amount = value;
-	}
-}
+ }
