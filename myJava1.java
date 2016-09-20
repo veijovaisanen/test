289992ee1e812122b9aa036514f77b1f4c9374dc
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2016-09-20 11:34:36 +0300

    Adden changePrice method

--- myJava1.java
+++ myJava1.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) { 
     	Product product = new Product("Edam", 3.3, 120);
+		product.changePrice(4.1);
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
      }
@@ -21,5 +22,9 @@ public class main {
  
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
+	}
+	
+	public void changePrice(int value) {
+		price = value;
 	}
 }
