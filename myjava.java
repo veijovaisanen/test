1bd98d07c24a1e25b749acb54fad20fe65b114b0
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2016-09-20 09:15:59 +0300

    Added price method

--- myjava.java
+++ myjava.java
@@ -3,6 +3,7 @@ public class main {
     public static void main(String[] args) { 
     	Product product = new Product("Edam", 3.3, 120);
 		product.changeBalance(200);
+		product.changePrice(4.1);
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
      }
@@ -23,6 +24,10 @@ public class main {
  
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
+	}
+
+	public void changePrice(int value) {
+		price = value;
 	}
 	
 	public void changeBalance(int value) {
